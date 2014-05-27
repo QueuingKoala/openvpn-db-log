29675b28410428db4515307f2712a7f2e965319d
Josh Cepek <josh.cepek@usa.net>
2014-05-27 00:13:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+﻿OpenVPN DB Log README
+=====================
+
+Overview
+--------
+
+This project logs OpenVPN connect / disconnect events to a database. The code is
+in Perl, so adding support for new backend databases is fairly simple.
+
+The OpenVPN DB Log project is licensed under the GPLv3 license:
+
+* http://opensource.org/licenses/GPL-3.0
+
+Current DB support
+------------------
+
+Multiple database backends are supported by available Perl DBI backends as
+available on the local system. The SQLite backend is the default if another
+backend is not specified as support is built-into recent Perl versions.
+
+Versions and schema stability
+-----------------------------
+
+Running release-versions is the safest way to go as this project won't guarantee
+schema stability for non-release versions.
+
+If you're not comfortable adjusting database schema between versions if
+necessary, a development branch is probably not for you.
+
---
+++ openvpn-db-log.pl
@@ -0,0 +1,162 @@
+#!/usr/bin/perl
+
+# Log openvpn connections to a DB.
+# Handles both connect and disconnect events, keyed by exposed env-vars.
+
+# Copyright Josh Cepek <josh.cepek@usa.net> 2014
+# Available under the GPLv3 license:
+# http://opensource.org/licenses/GPL-3.0
+
+use strict;
+use Getopt::Long;
+use DBI;
+
+my $db;
+my ($host, $port) = ('', '');
+my $user = undef;
+my $pass = undef;
+my $backend = "SQLite";
+my $rc_log_fail	= 0;
+GetOptions(
+	"fatal-failure|F!"	=> \$rc_log_fail,
+	"backend|b=s"		=> \$backend,
+	"database|db|d=s"	=> \$db,
+	"host|h=s"		=> \$host,
+	"user|u=s"		=> \$user,
+	"password|pass|p=s"	=> \$pass,
+);
+
+defined $db or
+	die "Options error: No database specified";
+
+# Define env-vars to check, and shorter reference option names.
+# Disconnect will add to this hash later if needed
+my %o = (
+	time		=> 'time_unix',
+	src_ip		=> 'trusted_ip',
+	src_port	=> 'trusted_port',
+	vpn_ip4		=> 'ifconfig_pool_remote_ip',
+	cn		=> 'common_name',
+);
+
+# Set var requirements and sub handler depending on script_type
+my $type = $ENV{script_type}
+	or die "ERR: script_type is unset";
+my $handler = \&connect;
+if ( $type =~ /^client-disconnect$/ ) {
+	$handler = \&disconnect;
+	# add some additional vars used during disconnect
+	%o = (
+		%o,
+		duration	=> 'time_duration',
+		bytes_in	=> 'bytes_received',
+		bytes_out	=> 'bytes_sent',
+	);
+}
+elsif ( $type !~ /^client-connect$/ ) {
+	die "Invalid script_type: '$type'";
+}
+
+# Verify and set env-var values
+# In each case, the actual value is assigned to %o.
+my $var;
+for my $key (keys %o) {
+	$var = $o{$key};
+	defined $ENV{$var}
+		or die "ERR: missing env-var: $var";
+	$o{$key} = $ENV{$var};
+}
+
+# On disconnect, the event time must be calculaed
+$type =~ /^client-disconnect$/
+	and $o{disconnect_time} = $o{time} + $o{duration};
+
+# Escape single-quotes in the cn as they're SQL string quotes:
+$o{cn} =~ s/'/''/g;
+
+# Connect to the SQL DB
+my $dbh;
+$dbh = DBI->connect(
+	"dbi:$backend:database=$db;host=$host;port=$port",
+	$user,
+	$pass, {
+		AutoCommit	=> 0,
+		RaiseError	=> 1,
+		PrintError	=> 0,
+	}
+);
+# Handle DB connect errors
+if ($@) {
+	die "DB connection failed: ($DBI::errstr)";
+}
+
+# Take the right DB update action depending on script type.
+# Any database errors escape the eval to be handled below.
+eval $handler->();
+
+# Handle any DB transaction errors from the handler sub
+if ($@) {
+	warn "SQL transaction failed: $@";
+	eval { $dbh->rollback; };
+	exit $rc_log_fail;
+}
+
+# Insert the connect data
+sub connect {
+	$dbh->do(qq{
+		INSERT INTO
+		session (
+			connect_time,
+			src_ip,
+			src_port,
+			vpn_ip4,
+			cn
+		)
+		VALUES (
+			'$o{time}',
+			'$o{src_ip}',
+			'$o{src_port}',
+			'$o{vpn_ip4}',
+			'$o{cn}'
+		)
+
+	});
+	$dbh->commit;
+}
+
+# Insert the disconnect data
+sub disconnect {
+	my $sth;
+	# Associate with the connect session using env-vars:
+	$sth = $dbh->prepare(qq{
+		SELECT	id
+		FROM	session
+		WHERE
+			disconnect_time is null
+		  AND	connect_time = '$o{time}'
+		  AND	vpn_ip4 = '$o{vpn_ip4}'
+		  AND	cn = '$o{cn}'
+		ORDER BY
+			id DESC
+		LIMIT	1
+	});
+	$sth->execute;
+	my $id = $sth->fetchrow_array
+		or die "No matching connection entry found";
+
+	# Update session details with disconnect values:
+	$sth = $dbh->prepare(qq{
+		UPDATE OR FAIL
+			session
+		SET
+			disconnect_time = '$o{disconnect_time}',
+			duration = '$o{duration}',
+			bytes_in = '$o{bytes_in}',
+			bytes_out = '$o{bytes_out}'
+		WHERE
+			id = '$id'
+	});
+	$sth->execute;
+	$dbh->commit;
+}
+
