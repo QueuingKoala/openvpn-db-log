8f48f45a15c7fa29223a96e2899418fa4e810844
Josh Cepek <josh.cepek@usa.net>
2014-08-09 21:32:09 -0500

    Remove usless rigid typing to SQLite schema
    SQLite does not use or care about rigid typing, so do without them in
    the column descriptions.

--- schema/sqlite.sql
+++ schema/sqlite.sql
@@ -1,23 +1,23 @@
 CREATE TABLE session (
 	id integer primary key autoincrement,
-	instance_id integer NOT NULL,
-	connect_time bigint NOT NULL,
-	src_ip varchar(39) NOT NULL,
-	src_port unsigned smallint NOT NULL,
-	cn varchar(64) NOT NULL,
-	vpn_ip4 varchar(15),
-	disconnect_time bigint,
-	duration integer,
-	bytes_in unsigned bigint,
-	bytes_out unsigned bigint,
+	instance_id int NOT NULL,
+	connect_time int NOT NULL,
+	src_ip text NOT NULL,
+	src_port int NOT NULL,
+	cn text NOT NULL,
+	vpn_ip4 text,
+	disconnect_time int,
+	duration int,
+	bytes_in int,
+	bytes_out int,
 	CONSTRAINT unique_session UNIQUE (instance_id, connect_time, src_ip, src_port, cn)
 );
 
 CREATE TABLE instance (
 	id integer primary key autoincrement,
-	name varchar(64) NOT NULL DEFAULT '',
-	port unsigned smallint NOT NULL DEFAULT 0,
-	protocol varchar(10) NOT NULL DEFAULT '',
+	name text NOT NULL DEFAULT '',
+	port int NOT NULL DEFAULT 0,
+	protocol text NOT NULL DEFAULT '',
 	CONSTRAINT unique_instance UNIQUE (name, port, protocol)
 );
 
